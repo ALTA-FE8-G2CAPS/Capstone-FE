dafce7e75a06b25dd609b7488596432998da478a
Muhamad-Fatah <muhamad.fatah494@gmail.com>
2022-09-30 10:36:59 +0700

    update login page

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,19 +1,26 @@
 import Image from "next/image"
 import React, { useState } from "react"
-import { Col, FloatingLabel, Form, Row } from "react-bootstrap"
+import { Col, FloatingLabel, Form, InputGroup, Row } from "react-bootstrap"
 import { MDBBtn } from 'mdb-react-ui-kit';
+import { BsFillEyeSlashFill, BsFillEyeFill } from "react-icons/bs"
 // Import Components
 import styles from "../styles/Login.module.css"
 
 export const Login = () => {
 
     const [test, setTest] = useState(false)
+    const [password, setPassword] = useState(true)
     const handleTest = () => {
         setTest(prev => !prev)
     }
 
-    const handleButton = e => {
+    const handleButton = (e) => {
         e.preventDefault()
+    }
+
+    const togglePassword = (e) => {
+        e.preventDefault()
+        setPassword(prev => !prev)
     }
 
     return (
@@ -28,17 +35,19 @@ export const Login = () => {
                 </Row>
                 <Col xl={5}>
                     <Row>
-                        <Image
-                            src="/segoro.png"
-                            width={105}
-                            height={64} />
-                    </Row>
-                    <Row>
                         <p className={styles.title}>Booking Now Enjoy Playing Later</p>
                     </Row>
                 </Col>
 
                 <Col className={styles.containerForm} xl={7}>
+                    <Row className={styles.logoBox}>
+                        <Image
+                            src="/segoro.png"
+                            width={230}
+                            height={150}
+                            className={styles.logo}
+                        />
+                    </Row>
                     {!test ? <Row className={styles.formBox}>
                         <h1 className={styles.loginTitle}>Login</h1>
                         <Form>
@@ -49,12 +58,21 @@ export const Login = () => {
                             >
                                 <Form.Control className={styles.formControl} type="email" placeholder="placeholder" />
                             </FloatingLabel>
-                            <FloatingLabel
-                                controlId="floatingPassword"
-                                label="Password"
-                            >
-                                <Form.Control className={styles.formControl} type="password" placeholder="placeholder" />
-                            </FloatingLabel>
+                            <InputGroup>
+                                <FloatingLabel
+                                    controlId="floatingPassword"
+                                    label="Password"
+                                >
+                                    <Form.Control
+                                        className={styles.formControl}
+                                        placeholder="placeholder"
+                                        type={password ? "password" : "text"}
+                                    />
+                                </FloatingLabel>
+                                <button className={styles.iconButton} onClick={(e) => togglePassword(e)}>
+                                    {password ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                                </button>
+                            </InputGroup>
                             <MDBBtn onClick={(e) => handleButton(e)} rippleColor='#A1D8D2' className={styles.loginButton}>
                                 Login
                             </MDBBtn>
@@ -80,13 +98,22 @@ export const Login = () => {
                                 >
                                     <Form.Control className={styles.formControl} type="email" placeholder="placeholder" />
                                 </FloatingLabel>
-                                <FloatingLabel
-                                    controlId="floatingPassword"
-                                    label="Password"
-                                >
-                                    <Form.Control className={styles.formControl} type="password" placeholder="placeholder" />
-                                </FloatingLabel>
-                                <MDBBtn onClick={(e) => handleButton(e)}  className={styles.loginButton}>
+                                <InputGroup>
+                                    <FloatingLabel
+                                        controlId="floatingPassword"
+                                        label="Password"
+                                    >
+                                        <Form.Control
+                                            className={styles.formControl}
+                                            placeholder="placeholder"
+                                            type={password ? "password" : "text"}
+                                        />
+                                    </FloatingLabel>
+                                    <button className={styles.iconButton} onClick={(e) => togglePassword(e)}>
+                                        {password ? <BsFillEyeSlashFill /> : <BsFillEyeFill />}
+                                    </button>
+                                </InputGroup>
+                                <MDBBtn onClick={(e) => handleButton(e)} className={styles.loginButton}>
                                     Register
                                 </MDBBtn>
                                 <p className={styles.validateText}>Already have account ?
