bb47df64da375cb27d8e4836b46d92bc5b4d1172
rofifatuzulfa <rofifatuz.rofzul@gmail.com>
2022-10-02 12:09:31 +0700

    add page review

--- pages/venue/review.jsx
+++ pages/venue/review.jsx
@@ -1,7 +1,106 @@
 import React from "react";
+import Image from "next/image";
+import { Col, OverlayTrigger, Row, Tooltip, Button } from "react-bootstrap";
+import { DetailHeading, DetailLayout } from "../../components/DetailLayout";
+import { AiFillStar } from "react-icons/ai";
+import { IoAddOutline } from "react-icons/io5";
+import styles from "../../styles/Review.module.css";
 
 const Review = () => {
-  return <div>Review</div>;
+  return (
+    <Row className={styles.container}>
+      <DetailLayout />
+      <Col md="12" lg="8">
+        <div className={styles.containerRight}>
+          <DetailHeading page="review" />
+          <div className={styles.reviewBody}>
+            <div className={styles.titleBody}>
+              <h5>Review :</h5>
+            </div>
+            <div>
+              <OverlayTrigger
+                key="top"
+                placement="top"
+                overlay={<Tooltip id={`tooltip-top`}>Add review ?</Tooltip>}
+              >
+                <Button variant="success" className={styles.buttonAdd}>
+                  <IoAddOutline size={35} />
+                </Button>
+              </OverlayTrigger>
+            </div>
+            <div className={styles.reviewBox}>
+              <Row className={styles.reviewItem}>
+                <div>
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                </div>
+                <div className={styles.reviewProfile}>
+                  <div>
+                    <Image
+                      className={styles.imageProfile}
+                      src="/profile.jpg"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className={styles.nameProfile}>
+                    <h6>Customer A</h6>
+                  </div>
+                </div>
+                <div>
+                  <p>Lapangannya nyaman kaya mantan</p>
+                </div>
+                <div className={styles.imageBox}>
+                  <Image
+                    className={styles.imageReview}
+                    src="/futsal.jpg"
+                    width={80}
+                    height={50}
+                  />
+                </div>
+              </Row>
+              <Row className={styles.reviewItem}>
+                <div>
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                  <AiFillStar size={30} className={styles.star} />
+                </div>
+                <div className={styles.reviewProfile}>
+                  <div>
+                    <Image
+                      className={styles.imageProfile}
+                      src="/profile.jpg"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className={styles.nameProfile}>
+                    <h6>Customer A</h6>
+                  </div>
+                </div>
+                <div>
+                  <p>Lapangannya nyaman kaya mantan</p>
+                </div>
+                <div className={styles.imageBox}>
+                  <Image
+                    className={styles.imageReview}
+                    src="/futsal.jpg"
+                    width={80}
+                    height={50}
+                  />
+                </div>
+              </Row>
+            </div>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
 };
 
 export default Review;
